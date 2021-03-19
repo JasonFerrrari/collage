4f6211dba15ad12f2cb141505b31184c1dfed1e5
JasonFerrrari <jason.ferrari@uconn.edu>
2021-03-19 17:10:44 -0400

    first commit

---
+++ data.js
@@ -0,0 +1,196 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-r0010009",
+      "name": "R0010009",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1418445093580196,
+          "pitch": 0.04115008335899262,
+          "rotation": 0,
+          "target": "3-r0010185_20210211093933"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-file_003",
+      "name": "File_003",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": -2.907428037703948,
+        "pitch": 0.0801230389481411,
+        "fov": 1.350113863664377
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.907428037703948,
+          "pitch": 0.0801230389481411,
+          "rotation": 0,
+          "target": "0-r0010009"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.1038456119011375,
+          "pitch": 0.505464517742034,
+          "title": "COVID-19 Protocols",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-r0010054",
+      "name": "R0010054",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.38978821836707667,
+          "pitch": 0.34870376827960214,
+          "rotation": 0,
+          "target": "1-file_003"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.5207734369635411,
+          "pitch": 0.5678906108315225,
+          "title": "Stitch",
+          "text": ""
+        },
+        {
+          "yaw": -0.03590854379542918,
+          "pitch": 1.3591138567967818,
+          "title": "(Unedited Version)",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-r0010185_20210211093933",
+      "name": "R0010185_20210211093933",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1197551818387943,
+          "pitch": 0.20335715614177374,
+          "rotation": 0,
+          "target": "2-r0010054"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.837148405528982,
+          "pitch": 0.2331470541815044,
+          "title": "Fireplace",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.7910131154435014,
+          "pitch": 0.07341445856996742,
+          "title": "Pool Table",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
